38e50061ef8b47573e2ced3d2e7112865ff8f9b0
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2023-07-13 14:02:12 -0300

    Model of Customer done

--- src/app/models/Customer.js
+++ src/app/models/Customer.js
@@ -14,33 +14,6 @@ class Customer extends Model {
           singular: 'customer',
           plural: 'customers',
         },
-        // scopes: {
-        //   active: {
-        //     where: {
-        //       status: 'ACTIVE',
-        //     },
-        //     order: ['createdAt'],
-        //   },
-        //   archived: {
-        //     where: {
-        //       status: 'ARCHIVED',
-        //     },
-        //   },
-        //   created(date) {
-        //     return {
-        //       where: {
-        //         createdAt: {
-        //           [Op.gte]: date,
-        //         },
-        //       },
-        //     };
-        //   },
-        // },
-        // hooks: {
-        //   beforeValidate: (customer) => {
-        //     customer.status = 'ACTIVE';
-        //   },
-        // },
       }
     );
   }
