479050ba9dab5de1557f5fdd561169707f34670a
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2023-07-14 17:15:33 -0300

    Starting contacts routes and controllers

---
+++ src/app/controllers/ContactsController.js
@@ -0,0 +1,121 @@
+import * as Yup from 'yup';
+import { Op } from 'sequelize';
+import { parseISO } from 'date-fns';
+
+import Customer from '../models/Customer';
+import Contact from '../models/Contact';
+
+class ContactsController {
+  async index(req, res) {
+    const {
+      name,
+      email,
+      status,
+      createdBefore,
+      createdAfter,
+      updatedBefore,
+      updatedAfter,
+      sort,
+    } = req.query;
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 25;
+
+    let where = {};
+    let order = [];
+
+    if (name) {
+      where = {
+        ...where,
+        name: {
+          [Op.iLike]: name,
+        },
+      };
+    }
+
+    if (email) {
+      where = {
+        ...where,
+        email: {
+          [Op.iLike]: email,
+        },
+      };
+    }
+
+    if (status) {
+      where = {
+        ...where,
+        status: {
+          [Op.in]: status.split(',').map((item) => item.toUpperCase()),
+        },
+      };
+    }
+
+    if (createdBefore) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.gte]: parseISO(createdBefore),
+        },
+      };
+    }
+
+    if (createdAfter) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.lte]: parseISO(createdAfter),
+        },
+      };
+    }
+
+    if (updatedBefore) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.gte]: parseISO(updatedBefore),
+        },
+      };
+    }
+
+    if (updatedAfter) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.lte]: parseISO(updatedAfter),
+        },
+      };
+    }
+
+    if (sort) {
+      order = sort.split(',').map((item) => item.split(':'));
+    }
+
+    const data = await Contact.findAll({
+      where,
+      include: [
+        {
+          model: Customer,
+          attributes: ['id', 'status'],
+          required: true,
+        },
+      ],
+      order,
+      limit,
+      offset: limit * page - limit,
+    });
+    return res.json(data);
+  }
+
+  async show(req, res) {
+    const contact = await Contact.findByPk(req.params.id);
+
+    if (!contact) {
+      return res.status(404).json();
+    }
+
+    return res.json(contact);
+  }
+}
+
+export default ContactsController;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import customers from './app/controllers/CustomersController';
+import contacts from './app/controllers/ContactsController';
 
 const routes = new Router();
 
@@ -9,5 +10,11 @@ routes.get('/customers/:id', customers.show);
 routes.post('/customers', customers.create);
 routes.put('/customers/:id', customers.update);
 routes.delete('/customers/:id', customers.destroy);
+
+routes.get('/customers/customersId/contacts', contacts.index);
+routes.get('/customers/:customersId/contacts/:id', contacts.show);
+routes.post('/customers/customersId/contacts', contacts.create);
+routes.put('/customers/:customersId/contacts/:id', contacts.update);
+routes.delete('/customers/:customersId/contacts/:id', contacts.destroy);
 
 export default routes;
