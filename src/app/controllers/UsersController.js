ec21efd372f1bdac6e87742bded3cc08545a9f65
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2023-07-17 18:39:16 -0300

    Starting developing of User Controller

--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -105,16 +105,21 @@ class UsersController {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().email().required(),
-      password_hash: Yup.string().email().required(),
+      password: Yup.string().required().min(8),
+      passwordConfirmation: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Error on validate schema.' });
     }
 
-    const user = await User.create(req.body);
+    const { id, name, email, createdAt, updatedAt } = await User.create(
+      req.body
+    );
 
-    return res.status(201).json(user);
+    return res.status(201).json({ id, name, email, createdAt, updatedAt });
   }
 
   async update(req, res) {}
