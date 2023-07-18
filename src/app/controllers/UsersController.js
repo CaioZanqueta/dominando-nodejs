0016cf64b82d52a93ecc4c628c3693341bee13b8
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2023-07-18 10:30:22 -0300

    User Controller Done

--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -98,7 +98,9 @@ class UsersController {
       return res.status(404).json();
     }
 
-    return res.json(user);
+    const { id, name, email, createdAt, updatedAt } = user;
+
+    return res.json({ id, name, email, createdAt, updatedAt });
   }
 
   async create(req, res) {
@@ -122,9 +124,55 @@ class UsersController {
     return res.status(201).json({ id, name, email, createdAt, updatedAt });
   }
 
-  async update(req, res) {}
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(8),
+      password: Yup.string()
+        .min(8)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      passwordConfirmation: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-  async destroy(req, res) {}
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Error on validate schema.' });
+    }
+
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).json();
+    }
+
+    const { oldPassword } = req.body;
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'User password not match.' });
+    }
+
+    const { id, name, email, createdAt, updatedAt } = await user.update(
+      req.body
+    );
+
+    return res.status(201).json({ id, name, email, createdAt, updatedAt });
+  }
+
+  async destroy(req, res) {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).json();
+    }
+
+    await user.destroy();
+
+    return res.json();
+  }
 }
 
 export default new UsersController();
